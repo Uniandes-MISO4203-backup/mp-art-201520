f9aa18d82a3499202c367ec86a31c36dab53fe11
Mario Lancheros <ms.lancheros10@uniandes.edu.co>
2015-09-23 19:42:41 -0500

    Req 3 issue fixed.

--- ArtMarketPlace.web/src/main/java/co/edu/uniandes/csw/artmarketplace/services/ArtistService.java
+++ ArtMarketPlace.web/src/main/java/co/edu/uniandes/csw/artmarketplace/services/ArtistService.java
@@ -3,6 +3,16 @@ package co.edu.uniandes.csw.artmarketplace.services;
 import co.edu.uniandes.csw.artmarketplace.api.IArtistLogic;
 import co.edu.uniandes.csw.artmarketplace.dtos.ArtistDTO;
 import co.edu.uniandes.csw.artmarketplace.providers.StatusCreated;
+import com.stormpath.sdk.account.Account;
+import com.stormpath.sdk.client.Client;
+import com.stormpath.sdk.client.Clients;
+import com.stormpath.sdk.api.ApiKeys;
+import com.stormpath.sdk.api.ApiKey;
+import com.stormpath.sdk.resource.ResourceException;
+import com.sun.media.jfxmedia.logging.Logger;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
@@ -17,6 +27,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import org.ini4j.Wini;
 
 /**
  * @generated
@@ -48,7 +59,33 @@ public class ArtistService {
         if (page != null && maxRecords != null) {
             this.response.setIntHeader("X-Total-Count", artistLogic.countArtists());
         }
-        return artistLogic.getArtists(page, maxRecords);
+        try {
+            URL url = ArtistService.class.getResource("ArtistService.class");
+            String className = url.getFile();
+            String filePath = className.substring(0,className.indexOf("WEB-INF") + "WEB-INF".length());
+            Wini ini = new Wini(new File(filePath+"/shiro.ini"));
+            String path = ini.get("main", "stormpathClient.apiKeyFileLocation");
+            ApiKey apiKey = ApiKeys.builder().setFileLocation(path).build();
+            Client client = Clients.builder().setApiKey(apiKey).build();
+            List<ArtistDTO> artists = artistLogic.getArtists(page, maxRecords);
+            for(ArtistDTO artistDTO:artists){
+                try {
+                   Account account = client.getResource(artistDTO.getUserId(), Account.class);
+                   artistDTO.setFirstName(account.getGivenName());
+                   artistDTO.setLastname(account.getSurname());
+                   artistDTO.setEmail(account.getEmail()); 
+                } catch (ResourceException e) {
+                    Logger.logMsg(Logger.ERROR, "The account with userid: "+artistDTO.getUserId()+" does not exist.");
+                }
+
+
+            }
+            return artists;
+        } catch (IOException e) {
+             Logger.logMsg(Logger.ERROR, e.getMessage());
+            return null;
+        }
+        
     }
 
     /**
