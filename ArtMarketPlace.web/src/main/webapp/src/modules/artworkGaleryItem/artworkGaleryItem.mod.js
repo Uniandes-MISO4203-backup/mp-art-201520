ee2e6bed7956209a2d4db3886459bedff4d3e131
unknown <jd.garcia1381@MISOEST-138.virtual.uniandes.edu.co>
2015-10-20 15:22:51 -0500

    Requerimiento Galería

---
+++ ArtMarketPlace.web/src/main/webapp/src/modules/artworkGaleryItem/artworkGaleryItem.mod.js
@@ -0,0 +1,48 @@
+(function (ng) {
+    var mod = ng.module('cartItemModule', ['ngCrud']);
+
+    mod.constant('cartItemContext', 'cartItems');
+
+    mod.constant('cartItemModel', {
+        fields: [{
+                name: 'name',
+                displayName: 'Name',
+                type: 'String',
+                required: true
+            },{
+                name: 'state',
+                displayName: 'State',
+                type: 'String',
+                required: true
+            }, {
+                name: 'quantity',
+                displayName: 'Quantity',
+                type: 'Integer',
+                required: true
+            }, {
+                name: 'artwork',
+                displayName: 'Artwork',
+                type: 'Reference',
+                service: 'artworkService',
+                options: [],
+                required: true
+            }, {
+                name: 'price',
+                displayName: 'Price',
+                type: 'Computed',
+                fn: function (record) {
+                    return record.artwork.price;
+                },
+                required: true,
+                editable: false,
+                currency: true
+            }, {
+                name: 'SubTotal',
+                displayName: 'SubTotal',
+                type: 'Computed',
+                editable: false,
+                fn: function (record) {
+                    return record.artwork.price * record.quantity;
+                }
+            }]});
+})(window.angular);
