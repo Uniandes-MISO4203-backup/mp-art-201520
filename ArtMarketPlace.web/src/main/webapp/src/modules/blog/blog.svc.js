a5975482c9fb5e134bb4020c30a342a8fe9b4dc9
jhrubiano10 <jh.rubiano10@uniandes.edu.co>
2015-10-20 11:42:45 -0500

    Deuda Técnica Blog

--- ArtMarketPlace.web/src/main/webapp/src/modules/blog/blog.svc.js
+++ ArtMarketPlace.web/src/main/webapp/src/modules/blog/blog.svc.js
@@ -3,24 +3,19 @@
     mod.service('blogService', ['CrudCreator', 'blogContext', function (CrudCreator, context) {
             CrudCreator.extendService(this, context);
             //Para saber el rol del usuario que está haciendo el proceso...
-            this.darRole = function()
-            {
-                return this.api.one('../users/currentUser').get();   
+            this.darRole = function(){
+                return this.api.one('../users/currentUser').get();
             };
-            
             //Trae todas las entradas del Blog dado el id del artista..
             this.allEntrys = function(idArtist){
                 return this.api.one('../resume/entryartist', idArtist).get();
             };
         }]);
-    
-    //Para invocar el servcio de creación de una nueva entrada al Blog... 
+    //Para invocar el servcio de creación de una nueva entrada al Blog.
     mod.service('newEntryService', ['CrudCreator', 'newEntryContext', function (CrudCreator, context) {
             CrudCreator.extendService(this, context);
-            this.saveEntry = function(data)
-            {
+            this.saveEntry = function(data){
                 return this.api.one("../resume/newentry").customPOST(data);
             };
-            
         }]);
 })(window.angular);
